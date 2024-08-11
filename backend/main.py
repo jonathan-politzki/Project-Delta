0835c8b1ebcbe010669c50074cc17bf3d395fbcf
Jonathan Politzki <91631593+jonathan-politzki@users.noreply.github.com>
2024-08-11 14:18:07 -0400

    Allow all origins for CORS

--- backend/main.py
+++ backend/main.py
@@ -1,29 +1,23 @@
 import os
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.endpoints.analysis import router as analysis_router
 import nltk
 nltk.data.path.append('./nltk_data')
 import uvicorn
 import logging
-from fastapi import Request
-
 
 logger = logging.getLogger(__name__)
 
 app = FastAPI(title="Writer Analysis Tool")
 
-# Use environment variable for CORS_ORIGINS
-CORS_ORIGINS = os.getenv("CORS_ORIGINS", "https://*.vercel.app,http://localhost:3000").split(",")
-
-logger.info(f"CORS_ORIGINS: {CORS_ORIGINS}")
-
+# Allow all origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=CORS_ORIGINS,
+    allow_origins=["*"],  # Allows all origins
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
 )
 
 app.include_router(analysis_router, prefix="/api/v1/analysis", tags=["analysis"])
@@ -33,8 +27,9 @@ async def root():
     return {"message": "Welcome to the Writer Analysis Tool API"}
 
 @app.middleware("http")
-async def log_requests(request, call_next):
+async def log_requests(request: Request, call_next):
     logger.info(f"Received request: {request.method} {request.url}")
+    logger.info(f"Request headers: {request.headers}")
     response = await call_next(request)
     logger.info(f"Returning response: {response.status_code}")
     return response
@@ -44,8 +39,6 @@ async def options_handler(request: Request, full_path: str):
     logger.info(f"Handling OPTIONS request for path: /{full_path}")
     logger.info(f"Request headers: {request.headers}")
     return {}
-
-
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
