aba52ad2a05f9fe67665ad58f6a75da9c909db6f
Jonathan Politzki <91631593+jonathan-politzki@users.noreply.github.com>
2024-08-11 11:44:03 -0400

    commit

--- backend/download_nltk_data.py
+++ backend/download_nltk_data.py
@@ -1,5 +1,7 @@
 import nltk
 
-nltk.download('punkt')
-nltk.download('vader_lexicon')
-nltk.download('stopwords')
+#nltk.download('punkt')
+#nltk.download('vader_lexicon')
+#nltk.download('stopwords')
+
+nltk.data.path.append('./nltk_data')
