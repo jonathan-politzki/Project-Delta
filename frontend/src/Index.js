19bc6ba5bf289030f549d8274e5f17e9b07d992b
Jonathan Politzki <91631593+jonathan-politzki@users.noreply.github.com>
2024-08-11 00:50:13 -0400

    Your commit message

--- frontend/src/Index.js
+++ frontend/src/Index.js
@@ -1,8 +1,10 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+console.log('index.js is being executed');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
