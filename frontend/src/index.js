6f3cdfc9f0af832c78856921d43aaa26115c2039
Jonathan Politzki <91631593+jonathan-politzki@users.noreply.github.com>
2024-08-11 01:07:43 -0400

    removing root bullshit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-console.log('index.js is being executed');
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-=======
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -28,5 +11,4 @@ root.render(
   </React.StrictMode>
 );
 
->>>>>>> 90fe35ee6c84883517f0f7c71259942815830d8c
 reportWebVitals();
