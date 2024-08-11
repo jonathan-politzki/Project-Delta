ff8c1c7adc6d60b81e005eda33d7ba41664ebe36
Jonathan Politzki <91631593+jonathan-politzki@users.noreply.github.com>
2024-08-11 13:50:43 -0400

    hopefully fixed env vars

--- backend/main.py
+++ backend/main.py
@@ -6,10 +6,14 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.endpoints.analysis import router as analysis_router
 import nltk
 nltk.data.path.append('./nltk_data')
+import uvicorn
 
 app = FastAPI(title="Writer Analysis Tool")
 
-CORS_ORIGINS = os.getenv("CORS_ORIGINS", "https://project-delta-anavc5x6c-jonathan-politzkis-projects.vercel.app/").split(",")
+CORS_ORIGINS = [
+    "https://*.vercel.app",
+    "http://localhost:3000"
+]
 
 app.add_middleware(
     CORSMiddleware,
@@ -26,6 +30,5 @@ async def root():
     return {"message": "Welcome to the Writer Analysis Tool API"}
 
 if __name__ == "__main__":
-    import uvicorn
     port = int(os.getenv("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
